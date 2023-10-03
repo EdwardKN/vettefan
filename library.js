2e05fe41749a2afe006ff42a759c6b631d6e2bb3
Edward <edward.klinghammer@gmail.com>
2023-10-03 15:11:16 +0200

    first commit

---
+++ library.js
@@ -0,0 +1,203 @@
+var canvas = document.createElement("canvas");
+var c = canvas.getContext("2d");
+
+var renderCanvas = document.createElement("canvas");
+var renderC = renderCanvas.getContext("2d");
+document.body.appendChild(renderCanvas);
+renderCanvas.style.zIndex = 0
+
+var scale = 0;
+
+window.onload = fixCanvas;
+
+window.addEventListener("resize", fixCanvas);
+
+renderCanvas.addEventListener("mousemove", function (e) {
+    let oldDown = mouse.down;
+    mouse = {
+        x: e.offsetX / scale,
+        y: e.offsetY / scale,
+        down: oldDown
+    };
+});
+
+var mouse = {
+    x: undefined,
+    y: undefined,
+    down: false
+};
+
+renderCanvas.addEventListener("mousedown", function (e) {
+    mouse.down = true;
+});
+renderCanvas.addEventListener("mouseup", function (e) {
+    mouse.down = false;
+});
+
+function fixCanvas() {
+    canvas.width = 1920 / 5;
+    canvas.height = 1080 / 5;
+    if (window.innerWidth * 9 > window.innerHeight * 16) {
+        renderCanvas.width = window.innerHeight * 16 / 9;
+        renderCanvas.height = window.innerHeight;
+        scale = renderCanvas.width / canvas.width;
+    } else {
+        renderCanvas.width = window.innerWidth;
+        renderCanvas.height = window.innerWidth * 9 / 16;
+        scale = renderCanvas.height / canvas.height;
+    };
+};
+
+
+
+var spritesheet;
+var spritesheetImage;
+
+async function loadSpriteSheet() {
+    var response = await fetch("./images/texture.json")
+    spritesheet = await response.json();
+    spritesheetImage = new Image();
+    spritesheetImage.src = "./images/texture.png";
+}
+
+async function loadImages(imageObject) {
+    await loadSpriteSheet();
+    Object.entries(imageObject).forEach(image => {
+        for (i = 0; i < image[1].length; i++) {
+            let src = (image[1][i].split("/"))
+            let src2 = src[src.length - 2] + "/" + src[src.length - 1]
+            image[1][i] = (spritesheet.frames[spritesheet.frames.map(function (e) { return e.filename; }).indexOf(src2 + ".png")])
+        }
+    });
+}
+
+function drawCircle(x, y, r, co) {
+    c.beginPath();
+    c.arc(x, y, r, 0, 2 * Math.PI, false);
+    c.fillStyle = co;
+    c.fill();
+}
+
+function detectCollision(x, y, w, h, x2, y2, w2, h2) {
+    if (x + w > x2 && x < x2 + w2 && y + h > y2 && y < y2 + h2) {
+        return true;
+    };
+};
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+};
+
+function drawLine(from, to, co) {
+    c.lineWidth = 2;
+    c.beginPath();
+    c.moveTo(from.x * tileSize, from.y * tileSize);
+    c.lineTo(to.x * tileSize, to.y * tileSize);
+    c.strokeStyle = co
+    c.stroke();
+}
+
+
+function pointCircleCollide(point, circle, r) {
+    if (r === 0) return false
+    var dx = circle[0] - point[0]
+    var dy = circle[1] - point[1]
+    return dx * dx + dy * dy <= r * r
+}
+
+var tmp = [0, 0]
+
+function lineCircleCollide(a, b, circle, radius, nearest) {
+    //check to see if start or end points lie within circle
+    if (pointCircleCollide(a, circle, radius)) {
+        if (nearest) {
+            nearest[0] = a[0]
+            nearest[1] = a[1]
+        }
+        return true
+    } if (pointCircleCollide(b, circle, radius)) {
+        if (nearest) {
+            nearest[0] = b[0]
+            nearest[1] = b[1]
+        }
+        return true
+    }
+
+    var x1 = a[0],
+        y1 = a[1],
+        x2 = b[0],
+        y2 = b[1],
+        cx = circle[0],
+        cy = circle[1]
+
+    //vector d
+    var dx = x2 - x1
+    var dy = y2 - y1
+
+    //vector lc
+    var lcx = cx - x1
+    var lcy = cy - y1
+
+    //project lc onto d, resulting in vector p
+    var dLen2 = dx * dx + dy * dy //len2 of d
+    var px = dx
+    var py = dy
+    if (dLen2 > 0) {
+        var dp = (lcx * dx + lcy * dy) / dLen2
+        px *= dp
+        py *= dp
+    }
+
+    if (!nearest)
+        nearest = tmp
+    nearest[0] = x1 + px
+    nearest[1] = y1 + py
+
+    //len2 of p
+    var pLen2 = px * px + py * py
+
+    //check collision
+    return pointCircleCollide(nearest, circle, radius)
+        && pLen2 <= dLen2 && (px * dx + py * dy) >= 0
+}
+
+function lineIntersect(a, b, c, d, p, q, r, s) {
+    var det, gamma, lambda;
+    det = (c - a) * (s - q) - (r - p) * (d - b);
+    if (det === 0) {
+        return false;
+    } else {
+        lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+        gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+        return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    }
+};
+
+function rectangleToLineIntersect(from, to, x, y, w, h) {
+    let collisionArray = [];
+    if (lineIntersect(from.x, from.y, to.x, to.y, x, y, x + w, y)) {
+        collisionArray.push("up")
+    }
+    if (lineIntersect(from.x, from.y, to.x, to.y, x, y, x, y + h)) {
+        collisionArray.push("left")
+    }
+    if (lineIntersect(from.x, from.y, to.x, to.y, x + w, y, x + w, y + h)) {
+        collisionArray.push("right")
+    }
+    if (lineIntersect(from.x, from.y, to.x, to.y, x, y + h, x + w, y + h)) {
+        collisionArray.push("down")
+    }
+    return collisionArray;
+}
+
+var pressedKeys = [];
+
+window.addEventListener('keydown', function (e) {
+    pressedKeys[e.code] = true;
+})
+
+window.addEventListener('keyup', function (e) {
+    pressedKeys[e.code] = false;
+})
---
+++ script.js
@@ -0,0 +1,177 @@
+var images = [];
+
+var map = [];
+
+var lines = [];
+
+var player = undefined;
+
+const mapSize = 15;
+const tileSize = 20;
+
+var currentEditingLine = undefined;
+
+async function init() {
+    initiateMap();
+    fixCanvas();
+    //await loadImages(images);
+    player = new Player(100, 100)
+    update();
+}
+
+function initiateMap() {
+    map = [];
+    for (let x = 0; x < mapSize; x++) {
+        let row = [];
+        for (let y = 0; y < mapSize; y++) {
+            row.push(new Point(x, y))
+        }
+        map.push(row);
+    }
+}
+
+function update() {
+    requestAnimationFrame(update);
+    renderC.clearRect(0, 0, renderCanvas.width, renderCanvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    render();
+
+    renderC.imageSmoothingEnabled = false;
+    renderC.drawImage(canvas, 0, 0, renderCanvas.width, renderCanvas.height);
+}
+
+function render() {
+    map.forEach(e => e.forEach(g => g.update()));
+
+    drawLineToMouse();
+
+    player.update();
+}
+
+function drawLineToMouse() {
+    if (currentEditingLine) {
+        drawLine({ x: currentEditingLine.x - player.x / tileSize, y: currentEditingLine.y - player.y / tileSize }, { x: mouse.x / tileSize, y: mouse.y / tileSize });
+    }
+}
+
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.color = "black";
+
+        this.connectedTo = [];
+    }
+
+    update() {
+        this.draw();
+        this.hover = (distance(this.x * tileSize - player.x, this.y * tileSize - player.y, mouse.x, mouse.y) < 5);
+
+        this.color = this.hover ? "gray" : "black";
+
+        if (this.hover && mouse.down && !currentEditingLine) {
+            currentEditingLine = this;
+            mouse.down = false;
+        } else if (this.hover && mouse.down && currentEditingLine !== this) {
+            let line = new Line(currentEditingLine, this);
+            currentEditingLine.connectedTo.push(line);
+            this.connectedTo.push(line);
+            lines.push(line);
+            currentEditingLine = undefined;
+            mouse.down = false;
+        } else if (this.hover && mouse.down && currentEditingLine == this) {
+            currentEditingLine = undefined;
+            mouse.down = false;
+        }
+        this.connectedTo.forEach(e => e.update());
+
+    }
+
+    draw() {
+        drawCircle(this.x * tileSize - player.x, this.y * tileSize - player.y, 2, this.color);
+    }
+}
+
+class Line {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+        this.color = "black";
+        this.hover = false;
+    }
+    update() {
+        this.hover = lineCircleCollide([this.from.x * tileSize - player.x, this.from.y * tileSize - player.x], [this.to.x * tileSize - player.x, this.to.y * tileSize - player.x], [mouse.x, mouse.y], 2);
+        this.color = this.hover ? "gray" : "black"
+        this.draw();
+    }
+    draw() {
+        drawLine({ x: this.from.x - player.x / tileSize, y: this.from.y - player.y / tileSize }, { x: this.to.x - player.x / tileSize, y: this.to.y - player.y / tileSize }, this.color);
+    }
+}
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = 10;
+        this.h = 20;
+        this.vx = 0;
+        this.vy = 0;
+        this.speedLoss = 0.9;
+        this.speedClamp = 0.5;
+    }
+    draw() {
+        c.fillStyle = "black"
+        c.fillRect(Math.floor(canvas.width / 2 - this.w / 2), Math.floor(canvas.height / 2 - this.h / 2), this.w, this.h)
+    }
+    update() {
+        this.updateVelocity();
+
+        this.x += this.vx;
+        this.y += this.vy;
+
+        this.checkCollisions();
+
+
+
+        this.draw()
+
+    }
+    updateVelocity() {
+        this.vx = this.vx > this.speedClamp ? this.speedClamp : (this.vx < -this.speedClamp ? -this.speedClamp : this.vx);
+        this.vy = this.vy > this.speedClamp ? this.speedClamp : (this.vy < -this.speedClamp ? -this.speedClamp : this.vy);
+        this.vx *= this.speedLoss;
+        this.vy *= this.speedLoss;
+        this.vx += (pressedKeys['KeyD'] ? 1 : (pressedKeys['KeyA'] ? -1 : 0));
+        this.vy += (pressedKeys['KeyS'] ? 1 : (pressedKeys['KeyW'] ? -1 : 0));
+    }
+    checkCollisions() {
+        lines.forEach(e => {
+            let collisionArray = rectangleToLineIntersect({ x: e.from.x * tileSize, y: e.from.y * tileSize }, { x: e.to.x * tileSize, y: e.to.y * tileSize }, this.x + Math.floor(canvas.width / 2 - this.w / 2), this.y + Math.floor(canvas.height / 2 - this.h / 2), this.w, this.h)
+            if (collisionArray.includes("up")) {
+                this.y++;
+                this.vy = 0;
+            }
+            if (collisionArray.includes("down")) {
+                this.y--;
+                this.vy = 0;
+            }
+            if (collisionArray.includes("left")) {
+                this.x++;
+                this.vx = 0;
+            }
+            if (collisionArray.includes("right")) {
+                this.x--;
+                this.vx = 0;
+            }
+        })
+    }
+
+}
+
+
+init();
